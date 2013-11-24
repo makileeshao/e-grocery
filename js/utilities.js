fa373973f82c7f25a99c8aabea6880bc8bf9cd8d
Pikachu <makilee.shao@gmail.com>
2013-11-24 11:52:48 +0800

    added info for Cordova and Phone

--- js/utilities.js
+++ js/utilities.js
@@ -7,3 +7,12 @@ function e_alert(message){
 // console.log(message);
 // console.error(message);
 
+function e_getDeviceInfo(){
+    var info = device.name + '<br />';
+    info = info + device.cordova + '<br />';
+    info = info + device.platform + '<br />';
+    info = info + device.uuid + '<br />';
+    info = info + device.version + '<br />';   
+    
+    return info;
+}
